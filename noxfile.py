594246b657747b38624b10adc5da15d5d767793d
georgedouzas <gdouzas@icloud.com>
2023-02-16 00:45:34 +0200

    CI Merged various tasks

--- noxfile.py
+++ noxfile.py
@@ -1,151 +1,135 @@
 """Development tasks."""
 
 import os
-from glob import glob
 from pathlib import Path
+from shutil import which
 
 import nox
 
 os.environ.update({'PDM_IGNORE_SAVED_PYTHON': '1', 'PDM_USE_VENV': '1'})
 
-PYTHON_VERSIONS = ['3.10', '3.11']
+PYTHON_VERSION_MIN, *_ = PYTHON_VERSIONS = ['3.10', '3.11']
 FILES = ['src', 'tests', 'docs', 'noxfile.py']
 
 
-@nox.session
-def build_docs(session: nox.Session) -> None:
-    """Build the documentation.
+def check_cli(session: nox.Session, args: list[str]) -> None:
+    """Check the CLI arguments.
+
+    Arguments:
+        session: The nox session.
+        args: The available CLI arguments.
+    """
+    if len(session.posargs) > 1 or session.posargs[0] not in args:
+        available_args = ', '.join([f'`{arg}`' for arg in args])
+        session.skip(
+            f'Available options should be one of {available_args}. Instead `{" ".join(session.posargs)}` was given',
+        )
+
+
+@nox.session(python=PYTHON_VERSION_MIN)
+def docs(session: nox.Session) -> None:
+    """Build, serve or deploy the documentation.
 
     Arguments:
         session: The nox session.
     """
-    session.run('mkdocs', 'build', external=True)
+    check_cli(session, ['serve', 'build', 'deploy'])
+    session.run('pdm', 'install', '-dG', 'docs', external=True)
+    if session.posargs[0] != 'deploy':
+        session.run('mkdocs', session.posargs[0])
+    else:
+        if which('vercel') is None:
+            session.skip('You should first install the `vercel` command')
+        session.run(
+            'vercel',
+            'pull',
+            '--yes',
+            '--environment=production',
+            '--token=${{ secrets.VERCEL_TOKEN }}',
+            external=True,
+        )
+        session.run('vercel', 'deploy', '--prod', '--token=${{ secrets.VERCEL_TOKEN }}', 'site', external=True)
 
 
-@nox.session
-def serve_docs(session: nox.Session) -> None:
-    """Serve the documentation.
-
-    Arguments:
-        session: The nox session.
-    """
-    session.run('mkdocs', 'serve', external=True)
-
-
-@nox.session
-def deploy_docs(session: nox.Session) -> None:
-    """Deploy the documentation.
-
-    Arguments:
-        session: The nox session.
-    """
-    session.run('mkdocs', 'gh-deploy', external=True)
-
-
-@nox.session
+@nox.session(python=PYTHON_VERSION_MIN)
 @nox.parametrize('file', FILES)
-def format_code(session: nox.Session, file: str) -> None:
+def formatting(session: nox.Session, file: str) -> None:
     """Format the code.
 
     Arguments:
         session: The nox session.
         file: The file to be formatted.
     """
-    session.run('black', file, external=True)
-    session.run('docformatter', '--in-place', '--recursive', '--close-quotes-on-newline', file, external=True)
+    check_cli(session, ['all', 'code', 'docstrings'])
+    session.run('pdm', 'install', '-dG', 'formatting', '--no-default', external=True)
+    if session.posargs[0] in ['code', 'all']:
+        session.run('black', file)
+    if session.posargs[0] in ['docstrings', 'all']:
+        session.run('docformatter', '--in-place', '--recursive', '--close-quotes-on-newline', file)
 
 
-@nox.session
+@nox.session(python=PYTHON_VERSION_MIN)
 @nox.parametrize('file', FILES)
-def check_quality(session: nox.Session, file: str) -> None:
-    """Check code quality.
+def checks(session: nox.Session, file: str) -> None:
+    """Check code quality, dependencies or type annotations.
 
     Arguments:
         session: The nox session.
         file: The file to be checked.
     """
-    session.run('ruff', file, external=True)
+    check_cli(session, ['all', 'quality', 'dependencies', 'types'])
+    session.run('pdm', 'install', '-dG', 'checks', '--no-default', external=True)
+    if session.posargs[0] in ['quality', 'all']:
+        session.run('ruff', file)
+    if session.posargs[0] in ['types', 'all']:
+        session.run('mypy', file)
+    if session.posargs[0] in ['dependencies', 'all']:
+        requirements_path = (Path(session.create_tmp()) / 'requirements.txt').as_posix()
+        session.run('pdm', 'export', '-f', 'requirements', '--without-hashes', '-o', requirements_path, external=True)
+        session.run('safety', 'check', '-r', requirements_path)
 
 
 @nox.session(python=PYTHON_VERSIONS)
-def check_dependencies(session: nox.Session) -> None:
-    """Check for vulnerabilities in dependencies.
+def tests(session: nox.Session) -> None:
+    """Run tests and coverage.
 
     Arguments:
         session: The nox session.
     """
-    requirements_path = (Path(session.create_tmp()) / 'requirements.txt').as_posix()
-    session.run('pdm', 'install', '-dG', 'check-dependencies', '--no-default', external=True)
-    session.run('pdm', 'export', '-f', 'requirements', '--without-hashes', '-o', requirements_path, external=True)
-    session.run('safety', 'check', '-r', requirements_path)
-
-
-@nox.session(python=PYTHON_VERSIONS)
-@nox.parametrize('file', ['src', 'tests', 'docs', 'noxfile.py'])
-def check_types(session: nox.Session, file: str) -> None:
-    """Check type annotations.
-
-    Arguments:
-        session: The nox session.
-        file: The file to be checked.
-    """
-    session.run('pdm', 'install', '-dG', 'check-types', '--no-default', external=True)
-    session.run('mypy', file)
-
-
-@nox.session(python=PYTHON_VERSIONS)
-def test(session: nox.Session) -> None:
-    """Run tests.
-
-    Arguments:
-        session: The nox session.
-    """
-    session.run('pdm', 'install', '-dG', 'test', external=True)
+    session.run('pdm', 'install', '-dG', 'tests', external=True)
     env = {'COVERAGE_FILE': f'.coverage.{session.python}'}
     if session.posargs:
         session.run('pytest', '-n', 'auto', '-k', *session.posargs, 'tests', env=env)
     else:
         session.run('pytest', '-n', 'auto', 'tests', env=env)
+    session.run('coverage', 'combine')
+    session.run('coverage', 'report')
+    session.run('coverage', 'html')
 
 
-@nox.session
-def coverage(session: nox.Session) -> None:
-    """Run test coverage.
-
-    Arguments:
-        session: The nox session.
-    """
-    if not glob('.coverage*'):
-        session.error(
-            'No coverage data were found. Please run `pdm test` to run the tests and generate the coverage data',
-        )
-    if glob('.coverage.*'):
-        session.run('coverage', 'combine', external=True)
-    session.run('coverage', 'report', external=True)
-    session.run('coverage', 'html', external=True)
-
-
-@nox.session
+@nox.session(python=PYTHON_VERSION_MIN)
 def changelog(session: nox.Session) -> None:
-    """Add news fragment to be used when chengelog is built.
+    """Add news fragment to be used when changelog is built.
 
     Arguments:
         session: The nox session.
     """
+    session.run('pdm', 'install', '-dG', 'changelog', external=True)
     import click
 
     issue_num = click.prompt('Issue number (start with + for orphan fragment)')
     frag_type = click.prompt('News fragment type', type=str)
-    session.run('towncrier', 'create', '--edit', f'{issue_num}.{frag_type}.txt', external=True)
+    session.run('towncrier', 'create', '--edit', f'{issue_num}.{frag_type}.txt')
 
 
-@nox.session
+@nox.session(python=PYTHON_VERSION_MIN)
 def release(session: nox.Session) -> None:
     """Kick off an automated release process.
 
     Arguments:
         session: The nox session.
     """
+    session.run('pdm', 'install', '-dG', 'release', external=True)
     import click
     from parver import Version
     from setuptools_scm import get_version
@@ -165,11 +149,11 @@ def release(session: nox.Session) -> None:
     )
     if not proceed:
         session.skip()
-    session.run('towncrier', 'build', '--yes', '--version', next_version, external=True)
-    session.run('git', 'add', 'CHANGELOG.md', external=True)
-    session.run('git', 'commit', '-m', f'Release {next_version}', external=True)
-    session.run('git', 'tag', '-a', next_version, external=True)
-    session.run('git', 'push', external=True)
-    session.run('git', 'push', '--tags', external=True)
+    session.run('towncrier', 'build', '--yes', '--version', next_version)
+    session.run('git', 'add', 'CHANGELOG.md')
+    session.run('git', 'commit', '-m', f'Release {next_version}')
+    session.run('git', 'tag', '-a', next_version, '-m', f'{increment_type.capitalize()} version number increment')
+    session.run('git', 'push')
+    session.run('git', 'push', '--tags')
     session.run('pdm', 'build', external=True)
-    session.run('twine', 'upload', '--skip-existing', 'dist/*', external=True)
+    session.run('twine', 'upload', '--skip-existing', 'dist/*')
